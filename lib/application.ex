89dceb4079672d16230b99b46951d08c97d9cf7c
Neven DREAN <nevendrean@yahoo.fr>
2024-07-12 11:03:25 +0200

    Update application.ex
    typo

--- lib/application.ex
+++ lib/application.ex
@@ -4,7 +4,7 @@ defmodule HlsDemo do
   use Application
 
   def start(_, _) do
-   ["priv/iinput", "priv/output", "priv/hls"] |> Enum.each(&File.mkdir/1)
+   ["priv/input", "priv/output", "priv/hls"] |> Enum.each(&File.mkdir/1)
     webserver = {Bandit, plug: MyWebRouter, port: 4000}
     Supervisor.start_link([webserver], strategy: :one_for_one)
   end
